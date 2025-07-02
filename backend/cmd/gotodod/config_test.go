849813451965d487063e1324dba3b8468de1cde8
Richard Baker <rich@mondaymorning.org>
2025-07-02 16:54:01 +0100

    test samesite config

---
+++ backend/cmd/gotodod/config_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func TestSessionSameSite1(t *testing.T) {
+	viper.Reset()
+	app := NewApplication()
+	t.Setenv("APP_ENV", "samesite1")
+	app.LoadConfig([]string{"-config", "./test-configs"})
+
+	expected := http.SameSiteStrictMode
+
+	if app.Config.Session.SameSite != expected {
+		t.Errorf("session same site wrong, expected \"%d\", got \"%d\"", expected, app.Config.Session.SameSite)
+	}
+}
+
+func TestSessionSameSite2(t *testing.T) {
+	viper.Reset()
+	app := NewApplication()
+	t.Setenv("APP_ENV", "samesite2")
+	app.LoadConfig([]string{"-config", "./test-configs"})
+
+	expected := http.SameSiteDefaultMode
+
+	if app.Config.Session.SameSite != expected {
+		t.Errorf("session same site wrong, expected \"%d\", got \"%d\"", expected, app.Config.Session.SameSite)
+	}
+}
+
+func TestSessionSameSite3(t *testing.T) {
+	viper.Reset()
+	app := NewApplication()
+	t.Setenv("APP_ENV", "samesite3")
+	app.LoadConfig([]string{"-config", "./test-configs"})
+
+	expected := http.SameSiteLaxMode
+
+	if app.Config.Session.SameSite != expected {
+		t.Errorf("session same site wrong, expected \"%d\", got \"%d\"", expected, app.Config.Session.SameSite)
+	}
+}
+
+func TestSessionSameSite4(t *testing.T) {
+	viper.Reset()
+	app := NewApplication()
+	t.Setenv("APP_ENV", "samesite4")
+	app.LoadConfig([]string{"-config", "./test-configs"})
+
+	expected := http.SameSiteNoneMode
+
+	if app.Config.Session.SameSite != expected {
+		t.Errorf("session same site wrong, expected \"%d\", got \"%d\"", expected, app.Config.Session.SameSite)
+	}
+}
+
+// samesite set to nonsense so should default to strict
+func TestSessionSameSite5(t *testing.T) {
+	viper.Reset()
+	app := NewApplication()
+	t.Setenv("APP_ENV", "samesite5")
+	app.LoadConfig([]string{"-config", "./test-configs"})
+
+	expected := http.SameSiteStrictMode
+
+	if app.Config.Session.SameSite != expected {
+		t.Errorf("session same site wrong, expected \"%d\", got \"%d\"", expected, app.Config.Session.SameSite)
+	}
+}
